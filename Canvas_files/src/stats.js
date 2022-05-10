5c4b960ff44532f81d7dc2451689ac69f6f995c3
cgccuser <91574512+cgccuser@users.noreply.github.com>
2022-05-10 17:45:24 -0400

    Create stats.js

---
+++ Canvas_files/src/stats.js
@@ -0,0 +1,60 @@
+let updateStats = (stats) => {
+    let placedPixels = document.getElementById("placed-pixels");
+    let totalPixels = document.getElementById("total-pixels");
+
+    placedPixels.innerText = Object.values(stats[config.iteration - 1]).reduce((a, b) => a + b, 0)
+    totalPixels.innerText = stats.flatMap(stat => Object.values(stat)).reduce((a, b) => a + b, 0)
+
+    let colourCounts = {};
+    for (let color of config.colors) {
+        for (let stat of stats) {
+            colourCounts[color] = (colourCounts[color] || 0) + stat[color];
+        }
+    }
+
+    let sortedColours = Object.entries(stats.at(-1)).sort((a, b) => b[1] - a[1]);
+    let topColours = sortedColours.slice(0, 5);
+    
+    let topColour = document.getElementById("favorite-colors");
+
+    topColour.innerHTML = "";
+
+    for (let [color, count] of topColours) {
+        let li = document.createElement("li");
+        let span = document.createElement("span");
+        span.style.backgroundColor = color;
+        span.style.color = "#888";
+        span.innerHTML = `&nbsp;`.repeat(5);
+        li.appendChild(span);
+
+        let text = document.createElement("span");
+        text.innerHTML = `&times;${count}`;
+        li.appendChild(text);
+
+        topColour.appendChild(li);
+    }
+
+    localStorage.setItem("stats", JSON.stringify(stats));
+}
+let initStats = () => {
+    let stats;
+    // Try grabbing stats from localstorage, if that fails initialize them
+    try {
+        stats = (JSON.parse(localStorage.getItem("stats")))
+    } catch (e) {}
+    if (!stats || !stats.length) {
+        stats = Array(config.iteration)
+    }
+    // Fill empty values
+    for(let i = 0; i < config.iteration; i++) {
+        stats[i] ||= {};
+        for(let color of config.colors) {
+            stats[i][color] ??= 0;
+        }
+    }
+    // Update stats
+    stats.update = updateStats.bind(null, stats)
+    return stats;
+}
+
+export default initStats;
